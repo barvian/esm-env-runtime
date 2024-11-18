ea7e5321d9c5d8fa9a89c7ffaeb59d2934bb6c45
Maxwell Barvian <max@barvian.me>
2024-11-18 04:38:23 -0800

    More runtime checks

--- dev.js
+++ dev.js
@@ -1,3 +1,3 @@
-export const BROWSER = false;
+export const BROWSER = typeof window !== "undefined";
 export const DEV = true;
 export const NODE = false;
--- node.js
+++ node.js
@@ -1,3 +1,3 @@
-export const BROWSER = false;
+export const BROWSER = typeof window !== "undefined";
 export const DEV = false;
 export const NODE = true;
